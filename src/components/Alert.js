2bcdb23e93f1f4318b0c10c7b2b727e93f3ee3ad
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-04-30 14:44:07 +0530

    Alert component Layout Shifting Problem Solved

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -10,8 +10,12 @@ export default function Alert(props) {
     //props.alert && acts like if else so if props.alert is null then the next will not render and it will not cause any error in the program.
     //JSX stands for javascript XML. it allows us to write HTML in React
     //js is written in className below to make the alert color button according to the alert type.
-    props.alert &&<div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-      <strong className='text-center'>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
+    //below div is used to solve alert component shifting the layout:
+
+    <div style={{height: '50px'}}>
+{        props.alert &&<div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+            <strong className='text-center'>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
+        </div>}
     </div>
   )
 }
